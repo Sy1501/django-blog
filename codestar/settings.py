3a23dc454be582c17398b0f3749db71681fec5e5
Simon Rutledge <sy1501@outlook.com>
2024-06-17 14:13:05 +0000

    added excerpt feild

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-sy1501-djangoblog-uflykan7gl9.ws-eu114.gitpod.io',
 '.herokuapp.com']
