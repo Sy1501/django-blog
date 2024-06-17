f9acbc179b79f73655da18e2e781901194d85f5d
Simon Rutledge <sy1501@outlook.com>
2024-06-17 10:24:54 +0000

    API

--- codestar/settings.py
+++ codestar/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-sy1501-djangoblog-uflykan7gl9.ws-eu114.gitpod.io',
+]
