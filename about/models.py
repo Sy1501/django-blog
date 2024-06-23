0bdb5378664eb0c6a77ef19922c3ad22ac3f2b26
Simon Rutledge <sy1501@outlook.com>
2024-06-23 17:20:13 +0000

    about page complete

--- about/models.py
+++ about/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated_on = models.DateTimeField(auto_now=True)
@@ -10,5 +9,6 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
 
 
