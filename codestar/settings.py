447698f0311e1db8a4b6fe80df8cf0928130dcc2
Simon Rutledge <sy1501@outlook.com>
2024-06-24 10:15:00 +0000

    added comment CRUD

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-sy1501-djangoblog-uflykan7gl9.ws-eu114.gitpod.io',
 '.herokuapp.com']
